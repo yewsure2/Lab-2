788b69d2a2d3ba613cef01e04b681fe15c09cfb8
yewcher <tyewcher.24@ichat.sp.edu.sg>
2025-05-05 10:25:38 +0800

    bmi is a new file

--- Lab2.py
+++ Lab2.py
@@ -1,4 +1,1 @@
-def calculate_bmi(height, weight):
-print("Height = " + height)
-print("Weight = " + weight)
-calculate_bmi(weight="57", height="1.73")
+print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
