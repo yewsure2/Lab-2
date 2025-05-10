2204a16b92986a4e3acdc2ed376228ad2fa6c200
yewcher <tyewcher.24@ichat.sp.edu.sg>
2025-05-10 12:15:26 +0800

    Lab2 has been modified

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,21 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("display_main_menu")
+    txt = "welcome to the jungle"
+
+    x = txt.split()
+
+    print(x)
+
+    thislist = ["apple", "banana", "cherry"]
+    print(type(thislist))
+
+def calc_average():
+    print("calc_average")
+
+def main():
+    display_main_menu()
+    calc_average()
+
+if __name__ == "__main__":
+    main()
